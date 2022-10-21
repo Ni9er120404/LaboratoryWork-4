93ae483d7f509f9e537fc57dd3a70aaeb8e38c5c
Даниил Лямин <Niger120404@yandex.ru>
2022-10-21 22:57:05 +0300

    Добавьте файлы проекта.

---
+++ LaboratoryWork-4/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace LaboratoryWork_4
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button1_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
